7aa87a9eefaa6039d71a42d670432362f3f55718
rafalsky94 <rafalbatorowicz94@gmail.com>
2017-09-19 19:38:28 +0200

    po dłuższej przerwie czas wrócić do projektu

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
+
+
 let swiper = new Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
-    paginationClickable: true
+    paginationClickable: true,
+    paginationBulletRender: function (swiper, index, className) {
+        const titles = ["01 INTRO", "02 WORK", "03 ABOUT", "04 CONTACTS"];
+        return '<span class="' + className + '">' + titles[index] + '</span>';
+    }
 });
 
 
